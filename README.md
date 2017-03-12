b2f3c1929655160a8e0f9f37b70405cc624823ae
Maxime Rihouey <maxime.rihouey@gmail.com>
2017-03-12 19:07:53 +0100

    multiple hash functions benchmark

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Count-Min-Sketch
 
+# Single hash function
 ![Benchmark](https://github.com/maximerihouey/count-min-sketch-python/blob/master/error_evolution.png)
+
+# Multiple hash functions
+![Benchmark](https://github.com/maximerihouey/count-min-sketch-python/blob/master/error_evolution_multiple.png)
--- count_min_sketch.py
+++ count_min_sketch.py
@@ -5,31 +5,31 @@ import random
 
 # Classes
 class HashFunction:
-    def __init__(self, a, b, size_hash_function):
-        self._a = a
-        self._b = b
+    def __init__(self, number_extra_hashing, size_hash_function):
+        self._number_extra_hashing = number_extra_hashing
         self._size_hash_function = size_hash_function
 
     def hash(self, element):
-        return (((hash(element) + self._a) % self._size_hash_function) + self._b) % self._size_hash_function
+        hash_value = hash(element)
+        for _ in range(self._number_extra_hashing):
+            hash_value = hash(str(hash_value))
+        return hash_value % self._size_hash_function
 
 class countMinSketch:
     def __init__(self, size_hash_function, number_hash_functions):
-        self._hash_functions = [new_hash_function(size_hash_function) for _ in range(number_hash_functions)]
-        self._count_min_sketch = defaultdict(lambda : 0)
+        self._hash_functions = [new_hash_function(i, size_hash_function) for i in range(number_hash_functions)]
+        self._count_min_sketches = [defaultdict(lambda : 0) for _ in range(number_hash_functions)]
 
     def update(self, element):
-        for hash_function in self._hash_functions:
-            self._count_min_sketch[hash_function.hash(element)] += 1
+        for index, hash_function in enumerate(self._hash_functions):
+            self._count_min_sketches[index][hash_function.hash(element)] += 1
 
     def get(self, element):
-        return min([self._count_min_sketch[hash_function.hash(element)] for hash_function in self._hash_functions])
+        return min([self._count_min_sketches[index][hash_function.hash(element)] for index, hash_function in enumerate(self._hash_functions)])
 
 # Helpers
-def new_hash_function(size_hash_function):
-    a, b = random.randint(1, size_hash_function), random.randint(1, size_hash_function)
-    print '=>', a, b, size_hash_function
-    return HashFunction(a, b, size_hash_function)
+def new_hash_function(number_extra_hashing, size_hash_function):
+    return HashFunction(number_extra_hashing, size_hash_function)
 #    return HashFunction(1, 0, size_hash_function)
 
 # Script
@@ -42,20 +42,22 @@ for line in f:
 f.close()
 
 # Count-Min-Sketch
-number_hash_functions = 2
-results = []
-for size_hash_function in range(1, 21):
-    count_min_sketch = countMinSketch(size_hash_function, number_hash_functions)
-    for element in elements:
-        count_min_sketch.update(element)
+for number_hash_functions in range(1, 10):
+    results = []
+    for size_hash_function in range(1, 100):
+        count_min_sketch = countMinSketch(size_hash_function, number_hash_functions)
+        for element in elements:
+            count_min_sketch.update(element)
 
-    # Counting error
-    error_by_key = [ count_min_sketch.get(key) / float(actual[key]) for key in actual.keys()]
-    print size_hash_function, np.mean(error_by_key)
-    results.append((size_hash_function, np.mean(error_by_key)))
+        # Counting error
+        error_by_key = [ count_min_sketch.get(key) / float(actual[key]) for key in actual.keys()]
+        # print size_hash_function, np.mean(error_by_key)
+        results.append((size_hash_function, np.mean(error_by_key)))
 
-plt.plot([a for a,b in results], [b for a,b in results])
+    plt.plot([a for a,b in results], [b for a,b in results], label=str(number_hash_functions)+' hash functions')
+
+plt.legend(loc=0)
 plt.xlabel('Size of Hash function')
 plt.ylabel('Mean error over the bins')
-plt.title('Evolution of the error of Count-Min-Sketch w.r.t. one hash function size')
+plt.title('Evolution of the error of Count-Min-Sketch w.r.t. several hash functions size')
 plt.show()
